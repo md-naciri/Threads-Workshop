72d1feb2f43f211908c4ddcf57acee191b368fa9
md-naciri <med.ar.naciri@gmail.com>
2025-02-08 20:47:48 +0100

    question 2.3 done

--- src/main/java/Sommeur.java
+++ src/main/java/Sommeur.java
@@ -1,7 +1,7 @@
 public class Sommeur implements Runnable{
     private int[] tableau;
     private int debut, fin;
-    private int somme;
+    private int somme = 0;
 
     public Sommeur(int[] tableau, int debut, int fin) {
         this.tableau = tableau;
@@ -9,8 +9,14 @@ public class Sommeur implements Runnable{
         this.fin = fin;
     }
 
+    public int getSomme() {
+        return somme;
+    }
+
     @Override
     public void run() {
-
+        for (int i = debut; i < fin; i++) {
+            somme += tableau[i];
+        }
     }
 }
