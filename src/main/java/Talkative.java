c409b9c7cc09267c42e0da6856723b7c76e9c5d1
md-naciri <med.ar.naciri@gmail.com>
2025-02-08 20:23:38 +0100

    question 1.2 done

--- src/main/java/Talkative.java
+++ src/main/java/Talkative.java
@@ -1,7 +1,12 @@
-public class Talkative {
+public class Talkative implements Runnable{
     private int number;
 
     public Talkative(int number) {
         this.number = number;
+    }
+
+    @Override
+    public void run() {
+
     }
 }
