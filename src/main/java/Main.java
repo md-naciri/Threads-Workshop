72a70949a47297b9d716b66d88d7f09d6886c956
md-naciri <med.ar.naciri@gmail.com>
2025-02-08 21:04:12 +0100

    question 2.4 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            Thread t = new Thread(new Talkative(i));
-            t.start();
+//        Exercice 1:
+//        for (int i = 0; i < 10; i++) {
+//            Thread t = new Thread(new Talkative(i));
+//            t.start();
+//        }
+
+//        Exercice 2:
+        int[] tableau = new int[100];
+        for (int i = 0; i < tableau.length; i++) {
+            tableau[i] = i+1;
         }
+
+
+
     }
 }
